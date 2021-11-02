0c346d3ebc7d4f5aeebb217535f1594fdae24087
AWS ParallelCluster user <ec2-user@ip-10-0-10-64.ap-southeast-1.compute.internal>
2021-11-02 20:30:23 +0000

    Add condaenv for rpllfp as well

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -17,14 +17,15 @@
 source ~/.bashrc
 envarg=`/data/src/PyHipp/envlist.py`
 conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
-import time; \
-pyh.RPLHighPass(saveLevel=1); \
-from PyHipp import mountain_batch; \
-mountain_batch.mountain_batch(); \
-from PyHipp import export_mountain_cells; \
-export_mountain_cells.export_mountain_cells(); \
-print(time.localtime());"
+	import time; \
+	pyh.RPLHighPass(saveLevel=1); \
+	from PyHipp import mountain_batch; \
+	mountain_batch.mountain_batch(); \
+	from PyHipp import export_mountain_cells; \
+	export_mountain_cells.export_mountain_cells(); \
+	print(time.localtime());"
 
 conda deactivate 
 /data/src/PyHipp/envlist.py $envarg
